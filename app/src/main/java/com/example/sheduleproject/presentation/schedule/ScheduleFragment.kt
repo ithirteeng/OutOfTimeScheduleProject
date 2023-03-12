ca01799600b1a575c5a5aad1eece450818b65207
Gulevskii Ivan <ithirteengw@gmail.com>
2023-03-12 15:57:38 +0700

    added navigation drawer and some logic for it

--- app/src/main/java/com/example/sheduleproject/presentation/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/example/sheduleproject/presentation/schedule/ScheduleFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GravityCompat
 import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
